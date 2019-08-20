c4e9b6ab4aed1f38d417d88d347993c2b8f8a071
Student <example@example.com>
2019-08-20 10:47:12 -0700

    add scopes.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,14 @@
 class Location < ApplicationRecord
-has_many :reviews, dependent: :destroy 
+has_many :reviews, dependent: :destroy
 validates :name, :place, :content, presence: true
 
-# scope :search_place, -> { (where(place: "#{location.place}"))}
+
+# scope :search_location, -> { (where(place: "#{location.place}"))}
+# scope :rando_destination, -> { (where(place: "#{location.random}"))}
+# scope :most_popular, -> { select("locations.id, locations.name, locations.cost, locations.origin, count(reviews.id) as reviews_count")
+#   .joins(:reviews)
+#   .group("locations.id")
+#   .order("reviews_count DESC")
+#   .limit(1)}
 
 end
