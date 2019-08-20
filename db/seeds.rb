81d717a17d640aec58ecc5fcaa0bcf73338fd375
Student <example@example.com>
2019-08-20 09:35:15 -0700

    add review seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 class Seed
 
   def self.begin
@@ -19,6 +12,13 @@ class Seed
         place: Faker::Address.state,
         content: Faker::Hipster.sentence
       )
+      7.times do |i|
+        reviews = location.reviews.create!(
+          author: Faker::Name.name,
+          content_body: Faker::Quote.famous_last_words,
+          rating: Faker::Number.within(range: 1..10)
+        )
+      end
       puts "Location #{i}: name is #{location.name} and content is '#{location.content}'"
     end
   end
